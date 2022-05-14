1162ef86768b4747db14095d91186c7cb057663c
denisdelaki <denisdelaki@gmail.com>
2022-05-14 06:49:25 +0300

    created the functions for different parts of the test

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,8 @@
 const authentication = document.getElementById("header");
 const quizes = document.getElementById("questions");
 const submittedResults = document.getElementById("results");
+// the authntication, quiz, and submit button functions
+function authent() {}
+function question() {}
+function results() {}
+//display the scored marks for the student immediately the submit buttton is clicked
