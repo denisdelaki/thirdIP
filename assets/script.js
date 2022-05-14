d3e8bcf93c1eca986b50af8d954c3bb56f3dbbcf
denisdelaki <denisdelaki@gmail.com>
2022-05-14 06:42:23 +0300

    declared and assignmed values to the variables

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+//declaring and assignment of variables
+const authentication = document.getElementById("header");
+const quizes = document.getElementById("questions");
+const submittedResults = document.getElementById("results");
