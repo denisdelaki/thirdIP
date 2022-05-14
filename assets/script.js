03b773a4619967c5657f066286f4ac12b1d65e98
denisdelaki <denisdelaki@gmail.com>
2022-05-14 10:23:17 +0300

    created a results function and the if parameter

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,65 @@
 //declaring and assignment of variables
-const authentication = document.getElementById("header");
-const quizes = document.getElementById("questions");
-const submittedResults = document.getElementById("results");
+//const authentication = document.getElementById("header");
+function results() {
+  var score = 0;
+  var Que1 = document.mcq.Q1.value;
+  var Que2 = document.mcq.Q2.value;
+  var Que3 = document.mcq.Q3.value;
+  var Que4 = document.mcq.Q4.value;
+  var Que5 = document.mcq.Q5.value;
+  var Que6 = document.mcq.Q6.value;
+  var Que7 = document.mcq.Q7.value;
+  var Que8 = document.mcq.Q8.value;
+  var Que9 = document.mcq.Q9.value;
+  var Que10 = document.mcq.Q10.value;
+  if ((Q1 = "Hyper Text Markup Language")) {
+    score++;
+  }
+  if ((Q2 = "Hyper Text Markup Language")) {
+    score++;
+  }
+  if ((Q3 = "Hyper Text Markup Language")) {
+    score++;
+  }
+  if ((Q4 = "Hyper Text Markup Language")) {
+    score++;
+  }
+  if ((Q5 = "Hyper Text Markup Language")) {
+    score++;
+  }
+  if ((Q6 = "Hyper Text Markup Language")) {
+    score++;
+  }
+  if ((Q7 = "Hyper Text Markup Language")) {
+    score++;
+  }
+  if ((Q8 = "Hyper Text Markup Language")) {
+    score++;
+  }
+  if ((Q9 = "Hyper Text Markup Language")) {
+    score++;
+  }
+  if ((Q10 = "Hyper Text Markup Language")) {
+    score++;
+  }
+  document.write(score);
+}
+
+/*const quizes = [
+  {
+    questions: " 1) HTML stands for?",
+    a: "Hyperactive Text Markup Language",
+    b: "Hyper Text Markup Language",
+    c: "Hyper Text Machine Language",
+    d: "None of the above.",
+    correct: "b",
+  },
+];
+//const submittedResults = document.getElementById("results");
 // the authntication, quiz, and submit button functions
+/*
 function authent() {}
 function question() {}
 function results() {}
 //display the scored marks for the student immediately the submit buttton is clicked
+submittedResults.addEventListener("click", results);*/
