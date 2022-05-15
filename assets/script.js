692c1e4c292f709744ac492da2da2769f8212e80
denisdelaki <denisdelaki@gmail.com>
2022-05-15 17:46:57 +0300

    declared variables

--- assets/script.js
+++ assets/script.js
@@ -1,118 +1,118 @@
-
 //generate the questions
 var allQuestions = [
   {
     question: " HTML stands for?",
-      options: {
-        A: "Hyperactive Text Markup Language",
-        B: "Hyper Text Markup Language",
-        C: "Hyper Text Machine Language",
-        D: "None of the above",
-      },
-      correctoption: "B",
+    options: {
+      A: "Hyperactive Text Markup Language",
+      B: "Hyper Text Markup Language",
+      C: "Hyper Text Machine Language",
+      D: "None of the above",
     },
- //question 2
-    {
-      question: "Which JavaScript method is used to write into an alert box?",
-      options: {
-        A: "window.alert().",
-        B: "window.alertContent()",
-        C: "window.alertBox()",
-        D: "window.alertHTML()",
-      },
-      correctoption: "A",
+    correctoption: "B",
   },
-    //Question 3
-    {
-      question: "What is the extension that a Javascript document is saved?",
-      options: {
-        A: ".html",
-        B: ".hml",
-        C: ".js",
-        D: ".css",
-      },
-      correctoption: "C",
-  },
-    //Question 4
-    {
-      Quiz: "Which of the following HTML tags will insert a line break?",
-      options: {
-        A: "<break>",
-        B: "<br>",
-        C: "<hb>",
-        D: "<p>",
-      },
-      correctoption: "B",
-  },
-    //Question5
-    {
-      Quiz: "Which one of the following is an ternary operator:",
-      options: {
-        A: "?",
-        B: ":",
-        C: "+",
-        D: "-",
-      },
-      correctoption: "A",
-  },
-    //Question6
-    {
-      Quiz: "Which type of Javascript language is?_______",
-      options: {
-        A: "High-level",
-        B: "Assembly-language",
-        C: "Object-Based",
-        D: "None of the above",
-      },
-      correctoption: "C",
-  },
-     //Questions7
-    {
-      Quiz:
-        "What symbols go around the properties for each Javascript function",
-      options: {
-        A: "<>",
-        B: "()",
-        C: "{}",
-        D: "[]",
-      },
-      correctoption: "C",
-  },
-    //Questions 8
-    {
-      Quiz: "The (function) and (var) are known as: (1 mrk)",
-      options: {
-        A: "Data Types",
-        B: "Keywords",
-        C: "Declaration statements",
-        D: "Prototypes",
-      },
-      correctoption: "C",
-  },
-    //Question9
-    {
-      Quiz:
-        "Which of the following variables takes precedence over the other if the names are the same?",
-      options: {
-        A: "Global variable",
-        B: "the local element",
-        C: "None of the above",
-      },
-      correctoption: "B",
-  },
-    //Question10
-    {
-      Quiz:
-        "Which one of the following is the correct way for calling the JavaScript code?",
-      options: {
-        A: "Function/method",
-        B: "Preprocessor",
-        C: "RMI",
-        D: "Triggering Event",
-      },
-      correctoption: "A",
+  //question 2
+  {
+    question: "Which JavaScript method is used to write into an alert box?",
+    options: {
+      A: "window.alert().",
+      B: "window.alertContent()",
+      C: "window.alertBox()",
+      D: "window.alertHTML()",
     },
+    correctoption: "A",
+  },
+  //Question 3
+  {
+    question: "What is the extension that a Javascript document is saved?",
+    options: {
+      A: ".html",
+      B: ".hml",
+      C: ".js",
+      D: ".css",
+    },
+    correctoption: "C",
+  },
+  //Question 4
+  {
+    Quiz: "Which of the following HTML tags will insert a line break?",
+    options: {
+      A: "<break>",
+      B: "<br>",
+      C: "<hb>",
+      D: "<p>",
+    },
+    correctoption: "B",
+  },
+  //Question5
+  {
+    Quiz: "Which one of the following is an ternary operator:",
+    options: {
+      A: "?",
+      B: ":",
+      C: "+",
+      D: "-",
+    },
+    correctoption: "A",
+  },
+  //Question6
+  {
+    Quiz: "Which type of Javascript language is?_______",
+    options: {
+      A: "High-level",
+      B: "Assembly-language",
+      C: "Object-Based",
+      D: "None of the above",
+    },
+    correctoption: "C",
+  },
+  //Questions7
+  {
+    Quiz: "What symbols go around the properties for each Javascript function",
+    options: {
+      A: "<>",
+      B: "()",
+      C: "{}",
+      D: "[]",
+    },
+    correctoption: "C",
+  },
+  //Questions 8
+  {
+    Quiz: "The (function) and (var) are known as: (1 mrk)",
+    options: {
+      A: "Data Types",
+      B: "Keywords",
+      C: "Declaration statements",
+      D: "Prototypes",
+    },
+    correctoption: "C",
+  },
+  //Question9
+  {
+    Quiz: "Which of the following variables takes precedence over the other if the names are the same?",
+    options: {
+      A: "Global variable",
+      B: "the local element",
+      C: "None of the above",
+    },
+    correctoption: "B",
+  },
+  //Question10
+  {
+    Quiz: "Which one of the following is the correct way for calling the JavaScript code?",
+    options: {
+      A: "Function/method",
+      B: "Preprocessor",
+      C: "RMI",
+      D: "Triggering Event",
+    },
+    correctoption: "A",
+  },
 ];
+//DECLARATION OF THE MAIN VARIABLES
+const quizContainer = document.getElementById("quiz");
+const resultsContainer = document.getElementById("results");
+const submitButton = document.getElementById("submit");
 /*function generatequestions() {
   var exam = [
     //Question 1
